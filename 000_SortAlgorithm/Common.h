21528a5d85a5046c1445cd5a1070ff88d5bf5948
NoMoreWaiting <ageofempires.universe@gmail.com>
2018-01-12 17:01:47 +0800

    add template func: ShowVector, Swap

--- 000_SortAlgorithm/Common.h
+++ 000_SortAlgorithm/Common.h
@@ -14,9 +14,16 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <vector>
 #include <iostream>
+
 using std::cout;
 using std::endl;
+using std::vector;
+
+
+
+// 打印数组元素
 
 void ShowArr(const int arr[], int len) {
     for (int i = 0; i < len; i++) {
@@ -29,8 +36,37 @@ void ShowArr(const int arr[], int len) {
 }
 
 
-void Swap(int &a, int &b){
+
+// 打印vector中元素的值
+
+template <typename T>
+void ShowVector(vector<T> & vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        if (0 != i) {
+            cout << ", ";
+        }
+        cout << vec[i];
+    }
+    cout << endl;
+}
+
+
+
+// 交换两个int类型的值
+
+void Swap(int &a, int &b) {
     int temp = a;
+    a = b;
+    b = temp;
+}
+
+
+// 注意模板类型必须有 复制构造函数 和 赋值重载函数
+// 交换两个模板类型的值
+
+template<typename T>
+void Swap(T &a, T&b) {
+    T temp = a;
     a = b;
     b = temp;
 }
