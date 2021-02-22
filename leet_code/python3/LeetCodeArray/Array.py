5cad1ebfa5fb911d9dc15adc7f578c80e160b9bd
hxw <NoMoreWaiting>
2021-02-22 17:05:49 +0800

    格式化 leet_code python 代码

--- leet_code/python3/LeetCodeArray/Array.py
+++ leet_code/python3/LeetCodeArray/Array.py
@@ -1,73 +1,75 @@
 class TwoSum:
-	"""
-	1. Two Sum
-	topics: array, hash table
-	"""
-	def twoSumHashMap(self, nums, target):
-		"""
-		:type nums: List[int]
-		:type target: int
-		:rtype: List[int]
-		"""
-		if len(nums) < 1:
-			return []
+    """
+    1. Two Sum
+    topics: array, hash table
+    """
 
-		numsDict = {}
+    def two_sum_hash_map(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) < 1:
+            return []
 
-		for i in range(len(nums)):
-			if (target - nums[i]) in numsDict:
-				return [numsDict[target - nums[i]], i]
-			numsDict[nums[i]] = i
+        nums_dict = {}
 
-		return []
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums_dict:
+                return [nums_dict[target - nums[i]], i]
+            nums_dict[nums[i]] = i
 
-	def test(self):
-		print("1. Two Sum")
-		nums = [3, 2, 4]
-		target = 6
-		res = self.twoSumHashMap(nums, target)
-		print("twoSumHashMap: ", res)
-		print()
+        return []
+
+    def test(self):
+        print("1. Two Sum")
+        nums = [3, 2, 4]
+        target = 6
+        res = self.two_sum_hash_map(nums, target)
+        print("twoSumHashMap: ", res)
+        print()
 
 
 class CombinationSum:
-	"""
-	39. CombinationSum
-	topics: array, backtracking
-	"""
-	def combinationSum(self, candidates, target):
-		"""
-		:type candidates: List[int]
-		:type target: int
-		:rtype: List[List[int]]
-		"""
-		res = []
-		list.sort(candidates)
-		self.combination(res, [], candidates, target, 0)
-		return res
+    """
+    39. CombinationSum
+    topics: array, backtracking
+    """
 
-	def combination(self, res, path, candidates, target, begin):
-		if target == 0:
-			res.append(path)
-			return
+    def combination_sum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        list.sort(candidates)
+        self.combination(res, [], candidates, target, 0)
+        return res
 
-		for key in range(begin, len(candidates)):
-			if target - candidates[key] >= 0:
-				self.combination(res, path + [candidates[key]], candidates, target - candidates[key], key)
-			else:
-				break
+    def combination(self, res, path, candidates, target, begin):
+        if target == 0:
+            res.append(path)
+            return
 
-	def test(self):
-		print("39. CombinationSum")
-		candidates = [2, 3, 5]
-		result = self.combinationSum(candidates, 8)
-		print("combinationSum: ", "candidates: ", candidates, "result: ", result)
-		print()
+        for key in range(begin, len(candidates)):
+            if target - candidates[key] >= 0:
+                self.combination(res, path + [candidates[key]], candidates, target - candidates[key], key)
+            else:
+                break
+
+    def test(self):
+        print("39. CombinationSum")
+        candidates = [2, 3, 5]
+        result = self.combination_sum(candidates, 8)
+        print("combinationSum: ", "candidates: ", candidates, "result: ", result)
+        print()
 
 
-def TestLeetCodeArray():
-	twoSum = TwoSum()
-	twoSum.test()
+def test_leet_code_array():
+    two_sum = TwoSum()
+    two_sum.test()
 
-	combination = CombinationSum()
-	combination.test()
+    combination = CombinationSum()
+    combination.test()
--- leet_code/python3/main.py
+++ leet_code/python3/main.py
@@ -2,9 +2,11 @@ import os
 import sys
 
 sys.path.append(os.getcwd() + "/")
-# print(sys.path)
-# import package.module as newName
 import LeetCodeArray.Array as Array
 
+# print(sys.path)
+# import package.module as newName
+
+
 if __name__ == "__main__":
-	Array.TestLeetCodeArray()
+    Array.test_leet_code_array()
